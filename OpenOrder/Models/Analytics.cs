3d9ee2d52bdfcd780e5ed79773c848a8d49aad46
Daniel Tabares <danieltabares@gmail.com>
2016-04-09 00:42:47 -0500

    Se versiona

---
+++ OpenOrder/Models/Analytics.cs
@@ -0,0 +1,15 @@
+﻿using OpenOrder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenOrder.Models
+{
+    public class AnalyticsViewModel
+    {
+        public List<OrderDateGroup> OrderData { get; set; }
+
+        public List<OrderDateGroup> OrderDataForToday { get; set; }
+    }
+}
---
+++ OpenOrder/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OpenOrder.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OpenOrder.ViewModel
+{
+    public class ShoppingCartViewModel
+    {
+        [Key]
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
